f24b53b1bbd6af699c8ed96b0d8ea531d5429434
John Kindem <461425614@qq.com>
2018-05-08 10:53:06 +0800

    * complete: 完成ChannelHeaderCard的动态效果

--- src/component/card/channel-header-card.js
+++ src/component/card/channel-header-card.js
@@ -9,7 +9,13 @@ export class ChannelHeaderCard extends React.Component {
         this.state = {
             attentionButtonActive: false,
             attentionButtonLoadDown: false,
-            userLogin: false
+            userLogin: false,
+
+            attentionNumLoadDown: false,
+            attentionNum: 0,
+
+            hotDegreeLoadDown: false,
+            hotDegree: 0
         }
     }
 
@@ -47,109 +53,149 @@ export class ChannelHeaderCard extends React.Component {
                             break;
                     }
                 }
+            });
+
+        // 发送请求获取频道的关注量
+        axios
+            .post('/request/channel/get_channel_attention_num', {
+                channel: this.props.channelId
             })
+            .then((response) => {
+                if (response.data.success) {
+                    this.setState({
+                        attentionNumLoadDown: true,
+                        attentionNum: response.data.num
+                    });
+                }
+            });
+
+        // 发送请求获取频道的热度
+        axios
+            .post('/request/channel/get_channel_hot_degree', {
+                channel: this.props.channelId
+            })
+            .then((response) => {
+                if (response.data.success) {
+                    this.setState({
+                        hotDegreeLoadDown: true,
+                        hotDegree: response.data.num
+                    });
+                }
+            });
     }
 
     render() {
-        return (
-            <Row style={{
-                backgroundColor: 'white',
-                padding: '20px 40px'
-            }}>
-                <Col span={5}>
-                    <img src={this.props.channelInfo.picture} className={'responsive-img'} style={{
-                        boxShadow: '0 0 10px #aaaaaa'
-                    }}/>
-                </Col>
-                <Col span={13} offset={1}>
-                    <div style={{
-                        fontWeight: 'bold',
-                        fontSize: '28px',
-                        color: 'black'
-                    }}>{this.props.channelInfo.name}&nbsp;</div>
-                    <p style={{
-                        fontSize: '15px',
-                        paddingTop: '5px'
-                    }}>{this.props.channelInfo.description}</p>
-                </Col>
-                <Col span={4} offset={1}>
-                    <Row>
-                        <Col span={10}>
-                            <div style={{
-                                fontWeight: 'bold',
-                                fontSize: '18px',
-                                float: 'right'
-                            }}>关注</div>
-                            <div style={{
-                                fontWeight: 'bold',
-                                fontSize: '25px',
-                                float: 'right'
-                            }}>123</div>
-                        </Col>
-                        <Col span={4}>
-                            <Divider type={'vertical'} style={{
-                                height: '60px',
-                                left: '20%'
-                            }}/>
-                        </Col>
-                        <Col span={10}>
-                            <div style={{
-                                fontWeight: 'bold',
-                                fontSize: '18px'
-                            }}>热度</div>
-                            <div style={{
-                                fontWeight: 'bold',
-                                fontSize: '25px'
-                            }}>789</div>
-                        </Col>
-                    </Row>
-                    <Row style={{
-                        marginTop: '20px'
-                    }}>
-                        <Col span={24}>
-                            {this.state.attentionButtonLoadDown?
-                                this.state.attentionButtonActive?(
-                                    <Button type={'primary'} style={{
-                                        width: '100%'
-                                    }} onClick={() => {
-                                        axios
-                                            .post('/request/channel/user_un_attention_channel', {
-                                                channel: this.props.channelId
-                                            })
-                                            .then((response) => {
-                                                if (response.data.success) {
-                                                    this.setState({
-                                                        attentionButtonActive: false
-                                                    });
-                                                }
-                                            })
-                                    }}>取消关注</Button>
-                                ):this.state.userLogin?
-                                    (<Button style={{
-                                        width: '100%'
-                                    }} onClick={() => {
-                                        axios
-                                            .post('/request/channel/user_attention_channel', {
-                                                channel: this.props.channelId
-                                            })
-                                            .then((response) => {
-                                                if (response.data.success) {
-                                                    this.setState({
-                                                        attentionButtonActive: true,
-                                                    });
-                                                }
-                                            })
-                                    }}>关注</Button>):
-                                    (<Button style={{
-                                        width: '100%'
-                                    }} onClick={() => {
-                                        this.props.history.push('/user/login')
-                                    }}>关注</Button>):null
+        if (this.state.hotDegreeLoadDown && this.state.attentionNumLoadDown && this.state.attentionButtonLoadDown) {
+            return (
+                <Row style={{
+                    backgroundColor: 'white',
+                    padding: '20px 40px'
+                }}>
+                    <Col span={5}>
+                        <img src={this.props.channelInfo.picture} className={'responsive-img'} style={{
+                            boxShadow: '0 0 10px #aaaaaa'
+                        }}/>
+                    </Col>
+                    <Col span={13} offset={1}>
+                        <div style={{
+                            fontWeight: 'bold',
+                            fontSize: '28px',
+                            color: 'black'
+                        }}>{this.props.channelInfo.name}&nbsp;</div>
+                        <p style={{
+                            fontSize: '15px',
+                            paddingTop: '5px'
+                        }}>{this.props.channelInfo.description}</p>
+                    </Col>
+                    <Col span={4} offset={1}>
+                        <Row>
+                            {this.state.attentionNumLoadDown?
+                                (<Col span={10}>
+                                    <div style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '18px',
+                                    }}>关注</div>
+                                    <div style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '25px',
+                                    }}>{this.state.attentionNum}</div>
+                                </Col>):null
                             }
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        );
+                            {this.state.attentionNumLoadDown&&this.state.hotDegreeLoadDown?
+                                (<Col span={4}>
+                                    <Divider type={'vertical'} style={{
+                                        height: '60px',
+                                        right: '50%'
+                                    }}/>
+                                </Col>):null
+                            }
+                            {this.state.hotDegreeLoadDown?
+                                (<Col span={10}>
+                                    <div style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '18px'
+                                    }}>热度</div>
+                                    <div style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '25px'
+                                    }}>{this.state.hotDegree}</div>
+                                </Col>):null
+                            }
+                        </Row>
+                        <Row style={{
+                            marginTop: '20px'
+                        }}>
+                            <Col span={24}>
+                                {this.state.attentionButtonLoadDown?
+                                    this.state.attentionButtonActive?(
+                                        <Button type={'primary'} style={{
+                                            width: '100%'
+                                        }} onClick={() => {
+                                            axios
+                                                .post('/request/channel/user_un_attention_channel', {
+                                                    channel: this.props.channelId
+                                                })
+                                                .then((response) => {
+                                                    if (response.data.success) {
+                                                        this.setState({
+                                                            attentionButtonActive: false
+                                                        });
+                                                    }
+                                                })
+                                        }}>取消关注</Button>
+                                    ):this.state.userLogin?
+                                        (<Button style={{
+                                            width: '100%'
+                                        }} onClick={() => {
+                                            axios
+                                                .post('/request/channel/user_attention_channel', {
+                                                    channel: this.props.channelId
+                                                })
+                                                .then((response) => {
+                                                    if (response.data.success) {
+                                                        this.setState({
+                                                            attentionButtonActive: true,
+                                                        });
+                                                    }
+                                                })
+                                        }}>关注</Button>):
+                                        (<Button style={{
+                                            width: '100%'
+                                        }} onClick={() => {
+                                            this.props.history.push('/user/login')
+                                        }}>关注</Button>):null
+                                }
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            );
+        } else {
+            return (
+                <div>
+
+                </div>
+            );
+        }
     }
 }
